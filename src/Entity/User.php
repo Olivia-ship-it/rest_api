93869e58d13599076b37f29b7a2a73a85cef64d9
Olivia WALSH <walsholivia7@gmail.com>
2021-05-03 21:32:27 +0200

    user controller done

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,9 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * 
  */
 class User
 {
@@ -20,7 +22,8 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * 
      */
     private $login;
 
